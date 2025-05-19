d7d86803ea63c5783923baa25f4b0d95260ef6a8
tiagosvieira10 <tiagosvieira10@gmail.com>
2025-05-19 14:25:03 -0300

    commit inicial

---
+++ src/main/java/br/com/tiagosvieira/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -0,0 +1,24 @@
+package br.com.tiagosvieira.gestao_vagas.modules.candidate.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.tiagosvieira.gestao_vagas.modules.candidate.CandidateEntity;
+import br.com.tiagosvieira.gestao_vagas.modules.candidate.CandidateRepository;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/candidate")
+public class CandidateController {
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    @PostMapping("/")
+    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        return this.candidateRepository.save(candidateEntity);
+    }
+}
