7474426fe99d13ec889397f9ff079379d1a7f6f7
tiagosvieira10 <tiagosvieira10@gmail.com>
2025-05-20 16:16:21 -0300

    company entity

---
+++ src/main/java/br/com/tiagosvieira/gestao_vagas/modules/company/entities/CompanyEntity.java
@@ -0,0 +1,42 @@
+package br.com.tiagosvieira.gestao_vagas.modules.company.entities;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Entity(name = "company")
+@Data
+public class CompanyEntity {
+
+  @Id
+  @GeneratedValue(strategy=GenerationType.UUID)
+  private UUID id;
+
+  @NotBlank()
+
+  @NotBlank
+  @Pattern(regexp = "\\S+", message = "O campo [username] nao deve conter espaços")
+  private String username;
+
+  @Email(message = "O campo deve conter um [email] valido")
+  private String email;
+
+  @Length(min = 10, max = 100, message = "A senha deve ter entre (10) e (100) caracteres")
+  private String password;
+
+  private String website;
+  private String name;
+  private String description;
+
+  private LocalDateTime createdAt;
+}
