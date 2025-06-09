2de0daf38308b9bdb67720838b452b883a8a61f4
tiagosvieira10 <tiagosvieira10@gmail.com>
2025-06-09 18:27:33 -0300

    add swagger no controller

--- src/main/java/br/com/tiagosvieira/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/tiagosvieira/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -18,50 +18,62 @@ import br.com.tiagosvieira.gestao_vagas.modules.candidate.useCases.CreateCandida
 import br.com.tiagosvieira.gestao_vagas.modules.candidate.useCases.ListAllJobsByFilterUseCase;
 import br.com.tiagosvieira.gestao_vagas.modules.candidate.useCases.ProfileCandidateUseCase;
 import br.com.tiagosvieira.gestao_vagas.modules.company.entities.JobEntity;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
+  @Autowired
+  private CreateCandidateUseCase createCandidateUseCase;
 
-    @Autowired
-    private CreateCandidateUseCase createCandidateUseCase;
+  @Autowired
+  private ProfileCandidateUseCase profileCandidateUseCase;
 
-    @Autowired
-    private ProfileCandidateUseCase profileCandidateUseCase;
+  @Autowired
+  private ListAllJobsByFilterUseCase listAllJobsByFilterUseCase;
 
-    @Autowired
-    private ListAllJobsByFilterUseCase listAllJobsByFilterUseCase;
-
-    @PostMapping("/")
-    public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
-        try {
-            var result = this.createCandidateUseCase.execute(candidateEntity);
-            return ResponseEntity.ok().body(result);
-        } catch (Exception e) {
-          return ResponseEntity.badRequest().body(e.getMessage());
-        }
+  @PostMapping("/")
+  public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
+    try {
+      var result = createCandidateUseCase.execute(candidateEntity);
+      return ResponseEntity.ok().body(result);
+    } catch (Exception e) {
+      return ResponseEntity.badRequest().body(e.getMessage());
     }
+  }
 
-    @GetMapping("/")
-    @PreAuthorize("hasRole('CANDIDATE')")
-    public ResponseEntity<Object> get(HttpServletRequest request) {
-        var idCandidate = request.getAttribute("candidate_id");
+  @GetMapping("/")
+  @PreAuthorize("hasRole('CANDIDATE')")
+  public ResponseEntity<Object> get(HttpServletRequest request) {
+    var idCandidate = request.getAttribute("candidate_id");
 
-        try {
-            var profile = this.profileCandidateUseCase
-                .execute(UUID.fromString(idCandidate.toString()));
-            return ResponseEntity.ok().body(profile);
-        } catch (Exception e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
-        }
+    try {
+      var profile = this.profileCandidateUseCase
+          .execute(UUID.fromString(idCandidate.toString()));
+      return ResponseEntity.ok().body(profile);
+    } catch (Exception e) {
+      return ResponseEntity.badRequest().body(e.getMessage());
     }
+  }
 
-    @GetMapping("/job")
-    @PreAuthorize("hasRole('CANDIDATE')")
-    public List<JobEntity> findJobByFilter(@RequestParam String filter) {
-        return this.listAllJobsByFilterUseCase.execute(filter);
-    }
-    
+  @GetMapping("/job")
+  @PreAuthorize("hasRole('CANDIDATE')")
+  @Tag(name = "Candidato", description = "Informações do candidato")
+  @Operation(summary = "Listagem de vagas disponível para o candidato", description = "Essa função é responsável por listar todas as vagas disponíveis, baseada no filtro")
+  @ApiResponses({
+      @ApiResponse(responseCode = "200", content = {
+          @Content(array = @ArraySchema(schema = @Schema(implementation = JobEntity.class)))
+      })
+  })
+  public List<JobEntity> findJobByFilter(@RequestParam String filter) {
+    return this.listAllJobsByFilterUseCase.execute(filter);
+  }
 }
