fbcf3cf391c9b5a3be4edeabf16e4f332b15eb1b
tiagosvieira10 <tiagosvieira10@gmail.com>
2025-06-09 17:37:05 -0300

    swagger configurado / useCases listagem de vagas

--- src/main/java/br/com/tiagosvieira/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/tiagosvieira/gestao_vagas/security/SecurityConfig.java
@@ -20,25 +20,30 @@ public class SecurityConfig {
   @Autowired
   private SecurityCandidateFilter securityCandidateFilter;
 
+  private static final String[] SWAGGER_LIST = {
+    "/swagger-ui/**",
+    "/v3/api-docs/**",
+    "/swagger-resource/**",
+  };
+
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.csrf(csrf -> csrf.disable())
-      .authorizeHttpRequests(auth -> {
-        auth.requestMatchers("/candidate/").permitAll()
-          .requestMatchers("/company/").permitAll()
-          .requestMatchers("/company/auth").permitAll()
-          .requestMatchers("/candidate/auth").permitAll();
-
-        auth.anyRequest().authenticated();
-      })
-      .addFilterBefore(securityCandidateFilter, BasicAuthenticationFilter.class)
-      .addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
-      return http.build();
+        .authorizeHttpRequests(auth -> {
+          auth.requestMatchers("/candidate/").permitAll()
+              .requestMatchers("/company/").permitAll()
+              .requestMatchers("/company/auth").permitAll()
+              .requestMatchers("/candidate/auth").permitAll()
+              .requestMatchers(SWAGGER_LIST).permitAll();
+          auth.anyRequest().authenticated();
+        })
+        .addFilterBefore(securityCandidateFilter, BasicAuthenticationFilter.class)
+        .addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
+    return http.build();
   }
 
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
-
 }
